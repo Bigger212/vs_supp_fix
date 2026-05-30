f6acf4362ad3e5386b1967f81b1c980d9fe6fca5
gabrom212 <gabrom212@gmail.com>
2026-05-30 19:31:12 -0400

    Recovered Mixin Class Files

---
+++ common/src/main/java/com/bigger212/mixin/CannonBallEntityPatch.java
@@ -0,0 +1,64 @@
+package com.bigger212.mixin;
+
+import java.util.Set;
+import net.bigger212.vs_sup_fix.vs_sup_fix;
+import net.mehvahdjukaar.supplementaries.common.entities.CannonBallEntity;
+import net.mehvahdjukaar.supplementaries.common.misc.explosion.CannonBallExplosion;
+import net.mehvahdjukaar.supplementaries.configs.CommonConfigs.Functional;
+import net.minecraft.core.BlockPos;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.phys.BlockHitResult;
+import net.minecraft.world.phys.Vec3;
+import org.joml.Vector3d;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.valkyrienskies.core.api.ships.Ship;
+import org.valkyrienskies.mod.common.VSGameUtilsKt;
+
+@Mixin({CannonBallEntity.class})
+public abstract class CannonBallEntityPatch {
+   public CannonBallEntityPatch() {
+   }
+
+   @Inject(
+      method = {"onHitBlock"},
+      at = {@At("HEAD")}
+   )
+   private void injectOnBlockHit(BlockHitResult result, CallbackInfo ci) {
+      CannonBallEntity self = (CannonBallEntity)this;
+      Level level = self.m_9236_();
+      if (level instanceof ServerLevel serverLevel) {
+         Vec3 hitPos = result.m_82450_();
+         Ship ship = VSGameUtilsKt.getShipObjectManagingPos(serverLevel, result.m_82425_());
+         if (ship != null) {
+            double radius;
+            if (vs_sup_fix.DAMAGE_SHIPS_UNIQUELY) {
+               radius = vs_sup_fix.CANNONBALL_BREAK_RADIUS;
+            } else {
+               try {
+                  radius = (Double)Functional.CANNONBALL_BREAK_RADIUS.get();
+               } catch (NullPointerException | NoSuchFieldError var20) {
+                  radius = vs_sup_fix.CANNONBALL_BREAK_RADIUS;
+               }
+            }
+
+            Vec3 movement = self.m_20184_();
+            double vel = Math.abs(movement.m_82553_());
+            float scaling = 5.0F;
+            float maxAmount = (float)(vel * vel * (double)scaling);
+            Vector3d hitPosJoml = new Vector3d(hitPos.f_82479_, hitPos.f_82480_, hitPos.f_82481_);
+            Vector3d shipHitPosJoml = ship.getWorldToShip().transformPosition(hitPosJoml);
+            Vec3 shipHitPos = new Vec3(shipHitPosJoml.x, shipHitPosJoml.y, shipHitPosJoml.z);
+            BlockPos shipCenter = new BlockPos((int)Math.floor(shipHitPos.f_82479_), (int)Math.floor(shipHitPos.f_82480_), (int)Math.floor(shipHitPos.f_82481_));
+            CannonBallExplosion explosion = new CannonBallExplosion(level, (Entity)null, shipHitPosJoml.x, shipHitPosJoml.y, shipHitPosJoml.z, shipCenter, maxAmount, (float)radius, (Set)null);
+            explosion.m_46061_();
+            explosion.m_46075_(true);
+         }
+
+      }
+   }
+}
---
+++ common/src/main/java/com/bigger212/mixin/ImprovedProjectileEntityPatch.java
@@ -0,0 +1,33 @@
+package com.bigger212.mixin;
+
+import net.mehvahdjukaar.moonlight.api.entity.ImprovedProjectileEntity;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.level.ClipContext;
+import net.minecraft.world.level.ClipContext.Block;
+import net.minecraft.world.level.ClipContext.Fluid;
+import net.minecraft.world.phys.BlockHitResult;
+import net.minecraft.world.phys.Vec3;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+import org.valkyrienskies.mod.common.world.RaycastUtilsKt;
+
+@Mixin({ImprovedProjectileEntity.class})
+public abstract class ImprovedProjectileEntityPatch {
+   public ImprovedProjectileEntityPatch() {
+   }
+
+   @Redirect(
+      method = {"Lnet/mehvahdjukaar/moonlight/api/entity/ImprovedProjectileEntity;move(Lnet/minecraft/world/entity/MoverType;Lnet/minecraft/world/phys/Vec3;)V"},
+      at = @At(
+   value = "INVOKE",
+   target = "Lnet/mehvahdjukaar/moonlight/api/util/math/MthUtils;collideWithSweptAABB(Lnet/minecraft/world/entity/Entity;Lnet/minecraft/world/phys/Vec3;D)Lnet/minecraft/world/phys/BlockHitResult;"
+)
+   )
+   private BlockHitResult redirectAABBCollision(Entity entity, Vec3 movement, double distance) {
+      Vec3 start = entity.m_20182_();
+      Vec3 end = start.m_82549_(movement);
+      ClipContext context = new ClipContext(start, end, Block.COLLIDER, Fluid.NONE, entity);
+      return RaycastUtilsKt.clipIncludeShips(entity.m_9236_(), context);
+   }
+}
